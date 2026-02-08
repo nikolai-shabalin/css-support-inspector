e515dbb932b03d7f752e20c9462d72cb8cfb9e76
Nikolai Shabalin <dev.nikolai.shabalin@gmail.com>
2026-02-08 20:16:22 +0300

    Update Astro configuration to support GitHub Actions with dynamic site and base URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,13 @@
 import { defineConfig } from 'astro/config';
 
-export default defineConfig({});
+const owner = process.env.GITHUB_REPOSITORY_OWNER;
+const repo = process.env.GITHUB_REPOSITORY?.split('/')[1];
+const isGithubActions = process.env.GITHUB_ACTIONS === 'true';
+
+const base = isGithubActions && repo ? `/${repo}` : '/';
+const site = isGithubActions && owner ? `https://${owner}.github.io` : undefined;
+
+export default defineConfig({
+  site,
+  base
+});
